e98a840980ac3cb11ecebec845c88a499c06ad3e
Thom Barrett <72460240+Thom440@users.noreply.github.com>
2021-01-26 09:53:06 -0800

    Issue#4 create product db (#7)
    * Created a product model
    
    * Set up DBContext
    
    * Added DbContext to startup. Added EF SQL Server NuGet package
    
    * Added database connection string
    
    * Added EFCore tools package
    
    * Added migration to create Database with products table

---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// A salable product
+    /// </summary>
+    public class Product
+    {
+        [Key] // Make Primary Key in database
+        public int ProductID { get; set; }
+
+        /// <summary>
+        /// The name of the product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The retail price of the product
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The category that the product falls into
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
