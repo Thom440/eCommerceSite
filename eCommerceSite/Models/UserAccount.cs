469e680704427bd9538d382c7cd54d6e914dc378
Thom Barrett <thom440@yahoo.com>
2021-02-16 08:30:21 -0800

    Created login page

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -50,4 +50,15 @@ namespace eCommerceSite.Models
         [StringLength(20)]
         public string Username { get; set; }
     }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username or Email")]
+        public string UsernameOrEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
 }
