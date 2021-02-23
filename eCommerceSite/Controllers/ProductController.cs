741944297fc4ce82f857f23f123534ba53e4d351
Thom Barrett <thom440@yahoo.com>
2021-02-23 12:54:53 -0800

    Refactored get single product db query

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -66,10 +66,7 @@ namespace eCommerceSite.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             // Get product with corresponding id
-            Product p =
-                await (from prod in _context.Products
-                        where prod.ProductID == id
-                        select prod).SingleAsync();
+            Product p = await ProductDB.GetProductAsync(_context, id);
 
             // Pass product to view
             return View(p);
@@ -92,9 +89,7 @@ namespace eCommerceSite.Controllers
         [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
-            Product p = await (from prod in _context.Products
-                         where prod.ProductID == id
-                         select prod).SingleAsync();
+            Product p = await ProductDB.GetProductAsync(_context, id);
 
             return View(p);
         }
@@ -103,9 +98,7 @@ namespace eCommerceSite.Controllers
         [ActionName("Delete")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            Product p = await (from prod in _context.Products
-                        where prod.ProductID == id
-                        select prod).SingleAsync();
+            Product p = await ProductDB.GetProductAsync(_context, id);
 
             _context.Entry(p).State = EntityState.Deleted;
             await _context.SaveChangesAsync();
